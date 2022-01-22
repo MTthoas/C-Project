b9b4f60e2281093d3a41c4cc2331d18cd7ab6d0d
MTthoas <matthias.pecquery@gmail.com>
2022-01-22 17:55:23 +0100

    my css now

--- main.c
+++ main.c
@@ -86,8 +86,8 @@ int main ( void )
     gtk_container_add ( GTK_CONTAINER ( window ), box );
 
     /// ***
-    main     = create_main     ( stack );
-    login_grid    = Login_page    ( stack );
+    main  = create_main  ( stack );
+    login_grid    = Login_page  ( stack );
     register_grid = Register_page ( stack );
 
     /// **
@@ -118,8 +118,8 @@ GtkWidget *create_main ( GtkWidget *stack )
     register_button = gtk_button_new_with_label ( "Register" );
 
     /// *** Add them to the Box
-    gtk_box_pack_start ( GTK_BOX ( box ), login_button,    0, 0, 0 );
-    gtk_box_pack_start ( GTK_BOX ( box ), register_button, 0, 0, 0 );
+    gtk_box_pack_start ( GTK_BOX ( box ), login_button,    TRUE, TRUE, 0 );
+    gtk_box_pack_start ( GTK_BOX ( box ), register_button, TRUE, TRUE, 0 );
 
     /// ***
     g_signal_connect ( login_button,    "clicked", G_CALLBACK ( login_clbk ),    stack );
@@ -131,7 +131,8 @@ GtkWidget *create_main ( GtkWidget *stack )
 
 GtkWidget *Login_page ( GtkWidget *stack )
 {
-    GtkWidget *grid;
+    GtkWidget *box;
+
     GtkWidget *login_button;
     GtkWidget *back_button;
 
@@ -139,28 +140,35 @@ GtkWidget *Login_page ( GtkWidget *stack )
     GtkWidget *label_password;
 
 
-    /// *** Create the Grid
-    grid = gtk_grid_new();
+    box = gtk_box_new ( GTK_ORIENTATION_VERTICAL, 5 );
 
-    /// ***
+    
     label_username = gtk_label_new ( "Username:" );
-    label_password = gtk_label_new ( "Password:" );
+    gtk_box_pack_start(GTK_BOX(box), label_username , TRUE, FALSE, 0);
 
-    /// ***
     entry_username = gtk_entry_new();
-    entry_password = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(box),   entry_username , TRUE, FALSE, 0);
 
-    /// ***
+
+
+    label_password = gtk_label_new ( "Password:" );
+    gtk_box_pack_start(GTK_BOX(box),  label_password , TRUE, FALSE, 0);
+
+     entry_password = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(box),   entry_password  , TRUE, FALSE, 0);
+
     login_button = gtk_button_new_with_label ( "Login" );
-    back_button  = gtk_button_new_with_label ( "Retour" );
+    gtk_box_pack_start(GTK_BOX(box),  login_button , TRUE, FALSE, 0);
+
+
+    back_button = gtk_button_new_with_label ( "Retour" );
+    gtk_box_pack_start(GTK_BOX(box),  back_button , TRUE, FALSE, 0);
 
     /// ***
-    gtk_grid_attach ( GTK_GRID ( grid ), label_username, 0, 0, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), entry_username, 1, 0, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), label_password, 0, 1, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), entry_password, 1, 1, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), back_button,    0, 2, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), login_button,   1, 2, 1, 1 );
+    // login_button = gtk_button_new_with_label ( "Login" );
+    // back_button  = gtk_button_new_with_label ( "Retour" );
+
+    /// ***
 
     /// ***
 
@@ -173,7 +181,7 @@ GtkWidget *Login_page ( GtkWidget *stack )
     g_signal_connect ( back_button, "clicked", G_CALLBACK ( main_clbk ), stack );
 
     /// ***
-    return grid;
+    return box;
 }
 
 
@@ -209,64 +217,74 @@ GtkWidget *create_hubby ( GtkWidget *stack ){
 
 GtkWidget *Register_page ( GtkWidget *stack )
 {
-    GtkWidget *grid;
+    GtkWidget *box;
     GtkWidget *register_button;
     GtkWidget *back_button;
 
-    GtkWidget *label_firstName;
-    GtkWidget *entry_firstName;
+    GtkWidget *label_username;
+    GtkWidget *entry_username;
 
-    GtkWidget *label_lastName;
-    GtkWidget *entry_lastName;
 
     GtkWidget *label_password;
     GtkWidget *entry_password;
 
-    GtkWidget *label_street;
-    GtkWidget *entry_street;
-
-    GtkWidget *label_number;
-    GtkWidget *entry_number;
 
     GtkWidget *label_email;
     GtkWidget *entry_email;
 
 
-    /// *** Create the Grid
-    grid = gtk_grid_new();
+    /// *** Create the BOX
+    box = gtk_box_new ( GTK_ORIENTATION_VERTICAL, 10 );
 
-    /// ***
-    label_firstName = gtk_label_new ( "First Name:" );
-    label_password  = gtk_label_new ( "Password:" );
-    label_email     = gtk_label_new ( "Email:" );
 
-    /// ***
-    entry_firstName = gtk_entry_new();
-    entry_password  = gtk_entry_new();
-    entry_email     = gtk_entry_new();
+    // FOR USERNAME 
 
-    /// ***
+
+    label_username = gtk_label_new ( "Username:" );
+    gtk_box_pack_start(GTK_BOX(box), label_username , TRUE, FALSE, 0);
+
+    entry_username = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(box),  entry_username  , TRUE, FALSE, 0);
+
+
+    // FOR EMAIL
+
+    label_email = gtk_label_new ( "Email:" );
+    gtk_box_pack_start(GTK_BOX(box), label_email, TRUE, FALSE, 0);
+
+    entry_email = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(box),  entry_email  , TRUE, FALSE, 0);
+
+
+    // FOR PASSWORD
+
+    label_password = gtk_label_new ( "Password:" );
+    gtk_box_pack_start(GTK_BOX(box), label_password, TRUE, FALSE, 0);
+
+    entry_password = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(box),  entry_password , TRUE, FALSE, 0);
+
+
+    // BUTTONS
+
     register_button = gtk_button_new_with_label ( "Register" );
-    back_button    = gtk_button_new_with_label ( "Retour" );
+    gtk_box_pack_start(GTK_BOX(box), register_button, TRUE, FALSE, 0);
 
-    /// ***
-    gtk_grid_attach ( GTK_GRID ( grid ), label_firstName, 0, 0, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), entry_firstName, 1, 0, 1, 1 );
 
-    gtk_grid_attach ( GTK_GRID ( grid ), label_password,  0, 2, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), entry_password,  1, 2, 1, 1 );
+    back_button = gtk_button_new_with_label ( "Retour" );
+    gtk_box_pack_start(GTK_BOX(box),  back_button, TRUE, FALSE, 0);
 
-    gtk_grid_attach ( GTK_GRID ( grid ), label_email,     0, 5, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), entry_email,     1, 5, 1, 1 );
 
-    gtk_grid_attach ( GTK_GRID ( grid ), back_button,     0, 6, 1, 1 );
-    gtk_grid_attach ( GTK_GRID ( grid ), register_button, 1, 6, 1, 1 );
+
+
+
+
 
     /// ***
     g_signal_connect ( back_button, "clicked", G_CALLBACK ( main_clbk ), stack );
 
     /// ***
-    return grid;
+    return box;
 }
 
 
