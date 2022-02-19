a73aaec8077aafbaed0fed96caadc2e9521a3c6c
MTthoas <matthias.pecquery@gmail.com>
2022-02-19 22:51:37 +0100

    Curling working

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 gcc -g trying.c -o trying.c_test `mysql_config --cflags --libs` 
-gcc -g `pkg-config --cflags gtk+-3.0` main.c -o helloworld `pkg-config --libs gtk+-3.0`
+gcc -g `pkg-config --cflags gtk+-3.0` main.c curlPages.c -o helloworld  `pkg-config --libs gtk+-3.0` -lcurl
 ./helloworld
 # ./trying.c_test
 
