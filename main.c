667b9a89b4638fb7ed8f303730816e8c080f313d
YukihiraDeve <valentinbourpro@gmail.com>
2022-03-02 10:58:43 +0100

    final

--- main.c
+++ main.c
@@ -17,21 +17,25 @@
 #include <stddef.h>
 
 #include "curlPages.h"
-#include "pid.h"
+
+#include <stdbool.h>
+#include <threads.h>
 
 
-int ConfigSetup();
-int terminal();
 
 void * create_main(GtkWidget * stack);
 GtkWidget * Login_page(GtkWidget * stack);
 GtkWidget * Register_page(GtkWidget * stack);
 void create_hubby(GtkWidget * stack);
 GtkWidget * choice_menu(GtkWidget * stack);
-// GtkWidget * time_menu(GtkWidget * stack);
+GtkWidget * time_menu(GtkWidget * stack);
 void quit_time_app(GtkWidget *w);
 void start_link(GtkButton * button);
+bool Exit = false;
 
+int CalculTime();
+char ExistingFile();
+int threadMain(void * needed);
 
 GtkWidget * object;
 
@@ -63,6 +67,7 @@ void create_time_app();
 void clicked_clbk(GtkButton * button, GtkStack * stack);
 void quit_clbk(void);
 
+int terminal();
 
 GtkWidget * entry_username;
 GtkWidget * entry_password;
@@ -84,15 +89,44 @@ static GtkWidget *pLabel;
 
 unsigned int mysql_result_nb = 0;
 
+int ConfigSetup(){
+    
+    
+    
+    FILE * f = NULL;
+
+    f = fopen("config.txt","r");
+    int tab[3]; 
+    char str1[3]="",str2[3]="",str3[3]="",str4[3]="";
+    if(f != NULL) {
+        fscanf(f, "%s %s", str1, str2);
+        tab[1]=atoi(str2);
+        tab[2]= atoi(str4);
+        
+        fclose(f);
+
+    } else {
+        printf("Impossible de lire le fichier");
+    }
+    
+    if (tab[1] == 1){
+        g_print("test");
+        return 0;
+    } else if(tab[1] == 0){
+        terminal();
+        return EXIT_SUCCESS;
+            }
+    // Graphique
+
+    // Crash app
+    // Nom de l'app
+}
 
 
-
-
-int main() {
+int main(void) {
 
     // system("firefox http://google.fr");
     
-   int StartProg = ConfigSetup();
 
     GtkWidget * window;
     GtkWidget * main;
@@ -104,6 +138,8 @@ int main() {
     GtkWidget * choice_grid;
     GtkWidget * time_grid;
 
+
+    ConfigSetup();
 
     
     // Initialisation
@@ -145,7 +181,7 @@ int main() {
     register_grid = Register_page(stack);
     // hubby_grid    = create_hubby  ( stack );
     choice_grid = choice_menu(stack);
-    // time_grid = time_menu(stack);
+    time_grid = time_menu(stack);
 
     /// **
     gtk_stack_add_named(GTK_STACK(stack), main, "Main");
@@ -282,28 +318,30 @@ GtkWidget * choice_menu(GtkWidget * stack) {
 
 }
 
-// GtkWidget * time_menu(GtkWidget * stack) {
+GtkWidget * time_menu(GtkWidget * stack) {
 
-//     GtkWidget * box;
-
-
-//     /// *** Create the Box
-//     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-
-//     GtkWidget * back_button;
+    GtkWidget * box;
 
 
-//     back_button = gtk_button_new_with_label("Retour");
-//     gtk_box_pack_start(GTK_BOX(box), back_button, TRUE, FALSE, 0);
+    /// *** Create the Box
+   box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
 
-//     g_signal_connect(back_button, "clicked", G_CALLBACK(choice_clbk), stack);
+    GtkWidget * back_button;
 
-
-//     return box;
-
+ 
 
 
-// }
+    back_button = gtk_button_new_with_label("Retour");
+    gtk_box_pack_start(GTK_BOX(box), back_button, TRUE, FALSE, 0);
+
+    g_signal_connect(back_button, "clicked", G_CALLBACK(choice_clbk), stack);
+
+
+   return box;
+
+
+
+ }
 
 void create_hubby(GtkWidget * stack) {
 
@@ -484,7 +522,6 @@ void start_link(GtkButton * button){
 
   strcpy(result, firefox);
   strcat(result, text);
-
     system(result);
     // system("firefox google.fr");
 
@@ -497,9 +534,12 @@ void create_time_app(){
     GtkWidget * box2;
 
     GtkWidget * label_description;
+    GtkWidget * Firefox;
     GtkWidget * back_button;
 
     window_app_time = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    ;
+
 
     gtk_window_set_title(GTK_WINDOW(window_app_time), "TIMMY");
     gtk_window_set_default_size(GTK_WINDOW(window_app_time), 300, 500);
@@ -514,6 +554,12 @@ void create_time_app(){
 
     label_description = gtk_label_new("Temps passé sur les applications");
     gtk_box_pack_start(GTK_BOX(box), label_description, TRUE, TRUE, 0);
+    
+   // Zone de texte.
+    
+   Firefox = gtk_label_new("Firefox %c");
+
+
 
     GtkWidget * scrolled_window;
 
@@ -1145,34 +1191,7 @@ int database(int proc, const gchar * user, const gchar * password, const gchar *
     }
 }
 
-int ConfigSetup(){
 
-    FILE * fichier = NULL;
-
-    fichier = fopen("config.txt","r");
-    int tab[3]; 
-    char str1[3]="",str2[3]="";
-    
-    if(fichier != NULL) {
-        fscanf(fichier, "%s %s", str1, str2);
-        tab[1]=atoi(str2);
-        fclose(fichier);
-
-    } else {
-        printf("Impossible de lire le fichier");
-    }
-    
-    if (tab[1] == 1){
-        return 1;
-    } else if(tab[1] == 0){
-        terminal();
-            }
-    // Graphique
-
-    // Crash app
-    // Nom de l'app
-
-}
 
 
 /* Déclaration des function*/
@@ -1436,4 +1455,218 @@ int terminal(){
 }
 
 
+
 }
+
+int CalculTime();
+char ExistingFile();
+int threadMain(void * needed);
+
+
+char ExistingFile(){
+
+  FILE *p;  
+  char *MYIDTXT = "./txt/3100.txt";
+
+  if( access (MYIDTXT, F_OK) != -1){
+
+    return true;
+
+  }else{
+
+
+    p = fopen(MYIDTXT, "w");
+                    
+    fprintf(p,"MOZILLA : Durée = 0\n");
+    fprintf(p,"2048 : Durée = 0\n");
+
+    fclose(p);
+
+return false;
+  }
+
+  return 0;
+
+}
+
+
+int threadMain(void * needed) {
+
+
+    FILE * p;
+    char ligne[1024];
+    char data[1024];
+    double time_diff_mozilla;
+    double time_diff_game;
+    double time_diff;
+    int index = 0;
+    int i;
+    bool block = false;
+    bool block_time = false;
+    int index_time = 0;
+    char *MYIDTXT = "./txt/3100.txt";
+    char *Environnement;
+
+
+    bool app_detected;
+    bool start_chrono = true;
+
+    int has_MOZILLA = 0, has_VSCODE = 0;
+
+    bool exist_file_for_this_id;
+
+    double data_from_mozilla;
+    double data_from_game;
+
+    time_t debut;
+    time_t end;
+
+
+
+    while (!Exit) {
+
+            if(needed == "/usr/lib/firefox-esr/firefox-esr" ){
+
+                p = popen("ps aux | grep firefox | tee ./txt/processes.txt", "r");
+                Environnement = "Mozilla";
+            }
+
+            if(needed == "gnome-2048"){
+
+                p = popen("ps aux | grep 2048 | tee ./txt/processes2048.txt", "r");
+                Environnement = "2048";
+                
+            }
+
+
+            if (!p) {
+                exit(-1);
+            }
+
+            while (fgets(ligne, sizeof(ligne), p)) {
+
+                index++;
+
+                if(index == 1){
+            
+
+                 if (strstr(ligne, needed) != NULL) {
+
+                     // L'application a été detecte
+
+                    // On démarre le chronomètre une seule fois et on informe au système que l'app a été detecté
+
+                    if(start_chrono == true){
+
+                         debut = time(NULL);
+                         printf("yes\n");
+                         app_detected = true;
+
+                    }
+
+                    start_chrono = false;
+          
+
+                 }else{
+
+                    // On sait que l'appli a été fermé et que l'app a été detecté
+                    // Donc on affiche le temps d'utilisation
+
+
+                     if(app_detected == true){
+
+                         end = time(NULL);
+
+                        time_diff = difftime(end, debut);
+
+                        // Récupération d'anciennes données
+
+                          p = fopen(MYIDTXT, "r");
+
+                            while (fgets(data, sizeof data, p)) {
+
+                                sscanf(data, "MOZILLA : Durée = %lf\n", &data_from_mozilla);
+
+                                sscanf(data, "2048 : Durée = %lf\n", &data_from_game);
+
+                            }
+
+                            fclose(p);
+
+                            p = fopen(MYIDTXT, "w");
+
+                            if(Environnement == "Mozilla"){
+                                  fprintf(p,"Mozilla : Durée = %lf \n", time_diff + data_from_mozilla);
+                                  fprintf(p,"2048 : Durée = %lf \n ", data_from_game);
+                            }
+                            
+                            if(Environnement == "2048"){
+                                fprintf(p,"Mozilla : Durée = %lf \n", data_from_mozilla);
+                                fprintf(p,"2048 : Durée = %lf \n ", time_diff + data_from_game);
+                            }
+
+
+                            fclose(p);
+                            printf("\n");
+
+
+                        app_detected = false;
+
+
+                     }
+                 }
+    
+            }
+
+    
+        }
+
+            
+        index = 0;
+
+    }
+
+    
+    printf("%s terminé !\n", data);
+    return thrd_success;
+}
+
+
+int CalculTime(){
+
+    ExistingFile();
+    // On crée un premier thread.
+    thrd_t thread1;
+
+    // Pid of Mozilla
+    void * MozillaFirefox = (void * )
+    "/usr/lib/firefox-esr/firefox-esr";
+
+    if (thrd_create( &thread1, threadMain, MozillaFirefox) != thrd_success) {
+        fprintf(stderr, "Impossible de créer le premier thread\n");
+        return EXIT_FAILURE;
+    }
+
+    // On crée un second thread.
+    thrd_t thread2;
+
+    // Pid of 2048
+    void * threadName2 = (void * )
+    "gnome-2048";
+
+    if (thrd_create( &thread2, threadMain, threadName2) != thrd_success) {
+        fprintf(stderr, "Impossible de créer le second thread\n");
+        return EXIT_FAILURE;
+    }
+
+    char ch;
+    scanf("%c", & ch);
+
+
+    Exit = true;
+
+    printf("Le thread initial/principal s'arrête.\n");
+    return EXIT_SUCCESS;
+
+};
+
